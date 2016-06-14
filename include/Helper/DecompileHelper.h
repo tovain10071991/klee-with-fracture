62e6ddf53345715a59d304e545cbb6018d5fd5d4
skyin <tovain1991@gmail.com>
2016-06-14 20:09:52 +0800

    modify dec_test to take 2 argvs directly

--- include/Helper/DecompileHelper.h
+++ include/Helper/DecompileHelper.h
@@ -10,5 +10,6 @@ llvm::Module* get_module(std::string binary);
 llvm::Function* get_first_func(unsigned long addr);
 llvm::Function* get_first_func(std::string func_name);
 llvm::Module* get_module_with_function(std::string func_name);
+llvm::Module* get_module_with_function(unsigned long addr);
 
 #endif
--- tools/test/dec_test.cpp
+++ tools/test/dec_test.cpp
@@ -1,4 +1,5 @@
 #include "Helper/DecompileHelper.h"
+#include "Helper/DebugHelper.h"
 
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
@@ -14,12 +15,14 @@ using namespace llvm;
 
 void handle_quit(const vector<string>& args);
 void handle_launch(const vector<string>& args);
-void handle_dec(const vector<string>& args);
+void handle_dec_by_name(const vector<string>& args);
+void handle_dec_by_addr(const vector<string>& args);
 
 map<string, void(*)(const vector<string>&)> cmd_func_map = {
   {"q", handle_quit},
   {"launch", handle_launch},
-  {"dec", handle_dec},
+  {"dec_by_name", handle_dec_by_name},
+  {"dec_by_addr", handle_dec_by_addr},
 };
 
 void print_prompt()
@@ -42,6 +45,15 @@ void handle_cmd(vector<string> strs)
 
 int main(int argc, char** argv)
 {
+  if(argc!=1)
+  {
+    create_debugger(argv[1]);
+    ::uint64_t addr = stoull(argv[2], 0, 0);
+    assert(addr);
+    get_module_with_function(addr);
+    cout << "dec done" << endl;
+    return 0;
+  }
   while(1)
   {
     print_prompt();
@@ -68,9 +80,17 @@ void handle_launch(const vector<string>& args)
   cout << "luanch done: " << init_mdl->getModuleIdentifier() << endl;
 }
 
-void handle_dec(const vector<string>& args)
+void handle_dec_by_name(const vector<string>& args)
 {
   assert(args.size()==1);
-  Module* mdl = get_module_with_function(args[0]);
+  get_module_with_function(args[0]);
+  cout << "dec done" << endl;
+}
+
+void handle_dec_by_addr(const vector<string>& args)
+{
+  assert(args.size()==1);
+  ::uint64_t addr = stoull(args[0], 0, 0);
+  get_module_with_function(addr);
   cout << "dec done" << endl;
 }
